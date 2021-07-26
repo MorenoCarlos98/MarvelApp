a7b4ef05a63900aa0cec68b430175fa759d557d1
Carlos <carlos.m.g.u@hotmail.com>
2021-07-26 09:43:39 +0200

    First commit. Added icon and splashscreen.

---
+++ app/src/main/java/com/carlos/marvelapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.carlos.marvelapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(1000)
+        setTheme(R.style.Theme_MarvelApp)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
